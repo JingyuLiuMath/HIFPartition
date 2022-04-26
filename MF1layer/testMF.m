6528d4bf5106b697a16fff0d632267d592f108a1
Jingyu <381258337@qq.com>
2022-04-26 15:46:11 +0800

    create MF1layer

---
+++ MF1layer/testMF.m
@@ -0,0 +1,61 @@
+%%
+% 1 - 5
+% |   |
+% 2 - 6
+% |   |
+% 3 - 7
+% |   |
+% 4 - 8
+
+A1 = [4,-1,0,0;
+    -1,4,-1,0;
+    0,-1,4,-1;
+    0,0,-1,4];
+A2 = [0,-1;
+    -1,0];
+I2 = eye(2);
+I4 = eye(4);
+A = kron(I2,A1) + kron(A2,I4);
+clear A1 A2 I2 I4;
+xy = [1,4;
+    1,3;
+    1,2;
+    1,1;
+    2,4;
+    2,3;
+    2,2;
+    2,1];
+Axy.A = A;
+Axy.xy = xy;
+
+MF = MFGraph(Axy);
+MF = BuildTree(MF);
+DemoFinalPart(MF);
+MF = SetNbNode(MF);
+MF = FillTree(MF);
+MF = Factorization(MF);
+x = ones(size(A,1),1);
+b = A*x;
+MF = MFSolve(MF,b);
+disp('Relative error:')
+disp(norm(MF.solution - x)/norm(x))
+
+%%
+[A,xy] = grid5(16);
+A = full(A);
+Axy.A = A;
+Axy.xy = xy;
+% Axy.xy = []
+
+MF = MFGraph(Axy);
+MF = BuildTree(MF);
+MF = SetNbNode(MF);
+% DemoPart(MF)
+% DemoFinalPart(MF);
+MF = FillTree(MF);
+MF = Factorization(MF,1);
+x = rand(size(A,1),1);
+b = A*x;
+MF = MFSolve(MF,b);
+disp('Relative error:')
+disp(norm(MF.solution - x)/norm(x))
